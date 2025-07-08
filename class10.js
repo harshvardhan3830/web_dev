ecde383cfa3e5c496afb66b0f34b8fc23e11b6c9
harshvardhan3830 <vardhan3830@gmail.com>
2025-07-08 20:53:49 +0530

    class 10

---
+++ class10.js
@@ -0,0 +1,20 @@
+// Data types -- 8 datatypes
+
+// String, Boolean, Number, BigINt, Undefined, null, symbol, objects
+
+// variable declaration
+// var, let and const 
+
+// var name = "Harshvardhan" //string
+// let city = "jaipur" //string
+// const age = 23 //number (Integer, Float)
+// const number = 1234567889 //BigInt
+// let isNew = false //boolean
+// let isTeacher = null
+// let kuchBhi = undefined
+
+// console.log(typeof(number))
+
+
+var age;
+console.log("Value of age is ===>> " ,age)
