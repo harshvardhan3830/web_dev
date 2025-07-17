56f0d3c44bdcd1a01a8ec57ad07211862c87d18a
harshvardhan3830 <vardhan3830@gmail.com>
2025-07-17 20:48:10 +0530

    Add class 11 and class 12 files covering conditionals, switch statements, loops, and data structures in JavaScript

---
+++ class11.js
@@ -0,0 +1,39 @@
+// conditionals
+
+// if and else 
+
+// Operators in JS
+// Comparision operators and always return boolean values
+// ==, ===, <= , >= , >, < , !=
+// let a = 5;
+// let b = "5";
+
+
+// if(a === b){
+//     console.log("Yes a is equals to b", a === b)
+// }else if(a == b){
+//     console.log("Value of a is equals to b")
+// }
+// else{
+//     console.log("No  a is not equals to b", a===b)
+// }
+
+
+// Switch
+let fruit = "mango"
+switch(fruit){
+    case "apple":
+        console.log("Fruit is apple")
+        break
+    case "lichi":
+        console.log("Fruit is lichi")
+        break
+    case "mango":
+        console.log("Fruit is mango")
+        break
+    default:
+        console.log("Fruit not in list")
+}
+
+// control statements
+// Break, Continue
---
+++ class12.js
@@ -0,0 +1,38 @@
+// Loops in JavaScript
+
+// For loop
+// for (initialization; condition; increment) {
+//     // code to be executed
+// }
+// for (let i = 0; i < 5; i++) {
+//     console.log("Iteration number:", i);
+// }
+
+
+// while loop
+// while (condition) {
+//     // code to be executed
+// }
+
+// let i = 0;
+// while (i < 5) {
+//     console.log("Iteration number:", i);
+//     i++;
+// }
+
+// Data Structures in JavaScript
+// Arrays
+// let fruits = ["apple", "banana", "mango", 10, true, null, undefined];
+// console.log("Fruits array:", typeof(fruits));
+// console.log("First fruit:", fruits[3]);
+
+// JSON (JavaScript Object Notation) (objects)
+
+let person = {
+    name : "Harshvardhan ",
+    age : 25,
+    city : "Delhi"
+}
+
+console.log("Person object:", person);
+console.log("Person's name:", person.city);
